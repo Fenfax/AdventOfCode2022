1dae9440ed3b2a93295da8b9efb3f607af7ed294
Fenfax <veitkull@hotmail.de>
2022-12-04 09:52:34 +0100

    Day04 part2

--- src/Day04.kt
+++ src/Day04.kt
@@ -2,15 +2,26 @@ fun main() {
     val parSplit = ','
     val rangeSplit = '-'
 
+    fun getRangeList(input: List<String>) = input
+        .map { fullString -> fullString.split(parSplit).map { it.split(rangeSplit) } }
+        .map { pairs -> pairs.map { it[0].toInt()..it[1].toInt() } }
+
     fun part1(input: List<String>): Int {
-        val splitInput = input
-            .map { fullString -> fullString.split(parSplit).map { it.split(rangeSplit) } }
-            .map { pairs -> pairs.map { it[0].toInt()..it[1].toInt() } }
+        val splitInput = getRangeList(input)
             .filter { (it[0].first in it[1] && it[0].last in it[1]) || (it[1].first in it[0] && it[1].last in it[0]) }
 
         return splitInput.size
     }
 
+    fun part2(input: List<String>): Int {
+        val splitInput = getRangeList(input)
+            .filter { it[0].first in it[1] || it[0].last in it[1] || it[1].first in it[0] || it[1].last in it[0] }
+
+        return splitInput.size
+    }
+
+
     val input = readInput("Day04")
     println(part1(input))
+    println(part2(input))
 }
