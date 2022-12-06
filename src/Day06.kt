730b426a2ab4717d900b59d49b4015bda48b7133
Fenfax <veitkull@hotmail.de>
2022-12-06 19:09:02 +0100

    Day06 compact solution

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,6 +1,6 @@
 fun main() {
 
-    fun findUnique(c: Char, queue: ArrayDeque<Char>, uniqueAmount: Int):Boolean{
+    fun findUnique(c: Char, queue: ArrayDeque<Char>, uniqueAmount: Int): Boolean {
         queue.add(c)
         if (queue.size >= uniqueAmount && queue.distinct().size == queue.size) {
             queue.clear()
@@ -17,14 +17,23 @@ fun main() {
 
         return input.map {
             it.toList()
-                .takeWhile { c -> findUnique(c,queue, uniqueAmount) }
+                .takeWhile { c -> findUnique(c, queue, uniqueAmount) }
                 .size + 1
         }.first()
 
     }
 
+    fun solveShort(input: String, uniqueAmount: Int): Int {
+        val work = input.toList()
+
+        return (1..input.length).toList()
+            .takeWhile { work.subList(it, it+ uniqueAmount).let {list -> list.size != list.distinct().size } }.last() + uniqueAmount + 1
+    }
+
 
     val input = readInput("Day06")
-    println(solve(input,4))
-    println(solve(input,14))
+    println(solve(input, 4))
+    println(solve(input, 14))
+    println(solveShort(input[0], 4))
+    println(solveShort(input[0], 14))
 }
