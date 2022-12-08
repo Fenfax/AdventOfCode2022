ec709ce0739e7eda3270680fe68218466ecdf712
Fenfax <veitkull@hotmail.de>
2022-12-08 22:20:58 +0100

    Day08 part2

--- src/Day08.kt
+++ src/Day08.kt
@@ -6,13 +6,52 @@ fun main() {
         for (tree in trees) {
             if (tree.treeHeight > currentHeight) {
                 currentHeight = tree.treeHeight
-                if(!tree.seen){
+                if (!tree.seen) {
                     visibleTrees = visibleTrees.inc()
                     tree.seen = true
                 }
             }
         }
         return visibleTrees
+    }
+
+    fun calcScore(trees: List<List<Tree>>, startingPoint: Pair<Int, Int>): Int {
+        var score = 1
+        var tempScore = 0
+        val treeSize = trees[startingPoint.first][startingPoint.second].treeHeight
+        for (x in (startingPoint.first + 1 until trees[startingPoint.first].size).toList()) {
+            tempScore = tempScore.inc()
+            if (trees[x][startingPoint.second].treeHeight >= treeSize) {
+                break
+            }
+        }
+        score *= tempScore
+        tempScore = 0
+        for (x in (startingPoint.first - 1 downTo 0).toList()) {
+            tempScore = tempScore.inc()
+            if (trees[x][startingPoint.second].treeHeight >= treeSize) {
+                break
+            }
+        }
+        score *= tempScore
+        tempScore = 0
+        for (y in (startingPoint.second + 1 until trees[startingPoint.first].size).toList()) {
+            tempScore = tempScore.inc()
+            if (trees[startingPoint.first][y].treeHeight >= treeSize) {
+                break
+            }
+        }
+        score *= tempScore
+        tempScore = 0
+        for (y in (startingPoint.second - 1 downTo 0).toList()) {
+            tempScore = tempScore.inc()
+            if (trees[startingPoint.first][y].treeHeight >= treeSize) {
+                break
+            }
+        }
+        score *= tempScore
+
+        return score
     }
 
 
@@ -27,8 +66,13 @@ fun main() {
                 4
     }
 
+    fun part2(input: List<List<Tree>>): Int {
+        return (input.indices).maxOf { (input[it].indices).maxOf { y -> calcScore(input, Pair(it, y)) } }
+    }
+
     val input = readInput("Day08").map { it.toList().map { c -> Tree(c.digitToInt()) } }
     println(part1(input))
+    println(part2(input))
 }
 
 data class Tree(val treeHeight: Int, var seen: Boolean = false)
