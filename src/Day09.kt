b9df05c76462622beed1a7d7b234809ce6ae0437
Fenfax <veitkull@hotmail.de>
2022-12-09 23:23:03 +0100

    Day09 part2

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,28 +1,48 @@
 import java.awt.Point
+import kotlin.math.abs
+import kotlin.math.sign
 
 fun main() {
-    fun part1(input: List<Pair<Direction, Int>>): Int {
-        val headPoint = Point()
-        val tailPoint = Point()
+    fun solve(input: List<Pair<Direction, Int>>, pointCount: Int): Int {
+        val points = (0 until pointCount).map { Point() }
 
-        val tailPointsVisited = mutableSetOf<Point>()
+        val tailPointsVisited = mutableSetOf(Point())
 
         for (command in input) {
             for (unused in (1..command.second)) {
-                val oldHead: Point = headPoint.clone() as Point
                 when (command.first) {
-                    Direction.R -> headPoint.translate(1, 0)
-                    Direction.L -> headPoint.translate(-1, 0)
-                    Direction.U -> headPoint.translate(0, 1)
-                    Direction.D -> headPoint.translate(0, -1)
+                    Direction.R -> points[0].translate(1, 0)
+                    Direction.L -> points[0].translate(-1, 0)
+                    Direction.U -> points[0].translate(0, 1)
+                    Direction.D -> points[0].translate(0, -1)
                 }
-                if(headPoint.distance(tailPoint) > 1.5){
-                    tailPointsVisited.add(tailPoint.clone() as Point)
-                    tailPoint.location = oldHead
+                for (i in (1 until pointCount)) {
+                    if (points[i - 1].distance(points[i]) > 1.5) {
+                        var translateX = 0
+                        var translateY = 0
+
+                        if (abs(points[i - 1].x - points[i].x) > 1) {
+                            translateX = points[i - 1].x - points[i].x
+                            if (abs(points[i - 1].y - points[i].y) > 0) {
+                                translateY = points[i - 1].y - points[i].y
+                            }
+                        }
+
+                        if (abs(points[i - 1].y - points[i].y) > 1) {
+                            translateY = points[i - 1].y - points[i].y
+                            if (abs(points[i - 1].x - points[i].x) > 0) {
+                                translateX = points[i - 1].x - points[i].x
+                            }
+                        }
+
+                        points[i].translate(translateX.sign, translateY.sign)
+                        if (i == pointCount - 1) {
+                            tailPointsVisited.add(Point(points[i]))
+                        }
+                    }
                 }
             }
         }
-        tailPointsVisited.add(tailPoint.clone() as Point)
 
         return tailPointsVisited.size
     }
@@ -30,7 +50,8 @@ fun main() {
 
     val input = readInput("Day09").map { cmd -> cmd.split(" ").let { Pair(Direction.valueOf(it[0]), it[1].toInt()) } }
 
-    println(part1(input))
+    println(solve(input, 2))
+    println(solve(input, 10))
 }
 
 private enum class Direction {
