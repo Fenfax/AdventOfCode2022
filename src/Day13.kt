22a2222e78d90be90d18b80538a70b9520f622e1
Fenfax <veitkull@hotmail.de>
2022-12-15 22:39:37 +0100

    Day13 part1

--- src/Day13.kt
+++ src/Day13.kt
@@ -1,32 +1,108 @@
 fun main() {
 
-    fun parseSingleSignal(s: String): Signal {
-        var signal = Signal(mutableListOf())
+    fun parseSingleSignal(s: String): Any {
+        val signal = mutableListOf<Any>()
 
         if (!s.contains("]") && !s.contains("[") && !s.contains(",")) {
-            return Signal(mutableListOf(), s.takeIf { it != "" }?.toInt() ?: 0)
+            return s.takeIf { it != "" }?.toInt() ?: Unit
         }
 
-        var currentString = s.substring(
-                s.indexOfFirst { it == '[' } + 1, s.indexOfLast { it == ']' }
+        val currentString = s.substring(
+            s.indexOfFirst { it == '[' } + 1, s.indexOfLast { it == ']' }
         )
 
-        //Problem : 1,[2,[3,[4,[5,6,7]]]],8,9  => maybe: (?<=^|\])[^[]
+        var start = 0
+        var openingCount = 0
+        var closingCount = 0
 
-        signal.multiple.addAll(currentString.split(Regex(",+(?![^\\[]*\\])")).map { parseSingleSignal(it) })
+        for (x in currentString.indices) {
+            if (currentString[x] == ',' && openingCount == closingCount) {
+                signal.add(parseSingleSignal(currentString.substring(start, x)))
+                start = x.inc()
+            }
+            if (currentString[x] == '[') {
+                openingCount += 1
+            }
+            if (currentString[x] == ']') {
+                closingCount += 1
+            }
+        }
+        start = currentString.length
+        for (x in currentString.indices.reversed()) {
+            if (currentString[x] == ',' && openingCount == closingCount) {
+                signal.add(parseSingleSignal(currentString.substring(x + 1, start)))
+                break
+            }
+            if (currentString[x] == '[') {
+                openingCount += 1
+            }
+            if (currentString[x] == ']') {
+                closingCount += 1
+            }
+            if (x == 0) {
+                signal.add(parseSingleSignal(currentString))
+            }
+        }
 
         return signal
     }
 
-    fun part1(input: List<String>): Int {
-        val signalList: List<Pair<Signal, Signal>> = input.splitWhen { it == "" }.map { Pair(parseSingleSignal(it[0]), parseSingleSignal(it[1])) }
+    fun compareSignal(signalPair: Pair<Any, Any>): Boolean? {
 
-        return input.size
+        if (signalPair.first is Int && signalPair.second is Int) {
+            if (signalPair.first == signalPair.second) {
+                return null
+            }
+            return (signalPair.first as Int) < (signalPair.second as Int)
+        }
+
+        if (signalPair.first is List<*> && signalPair.second is List<*>) {
+            val signalListOne = (signalPair.first as List<*>)
+            val signalListTwo = (signalPair.second as List<*>)
+
+            val test = (signalListOne.indices)
+                .asSequence()
+                .map {
+                    if (!signalListTwo.indices.contains(it)) {
+                        false
+                    } else {
+                        compareSignal(Pair(signalListOne[it]!!, signalListTwo[it]!!))
+                    }
+                }
+                .takeWhileInclusive { it == null }
+                .toList()
+
+            return test.firstNotNullOfOrNull { it } ?: if (signalListOne.size >= signalListTwo.size) return null else return true
+
+        }
+
+        if (signalPair.second !is List<*>) {
+            return compareSignal(Pair(signalPair.first, listOf(signalPair.second as Int)))
+        }
+
+        return compareSignal(Pair(listOf(signalPair.first as Int), signalPair.second))
+
     }
 
-    val input = readInput("Day13_test")
+    fun part1(input: List<String>): Int {
+        val signalList: List<Pair<Any, Any>> = input.splitWhen { it == "" }.map { Pair(parseSingleSignal(it[0]), parseSingleSignal(it[1])) }
+
+        val signalValidation = signalList.map {
+            compareSignal(it)
+        }
+        return signalValidation.foldIndexed(0) { index, acc, b -> if (b != false) acc + index + 1 else acc }
+    }
+
+    val input = readInput("Day13")
     println(part1(input))
 }
 
-
-data class Signal(val multiple: MutableList<Signal>, var single: Int? = null)
+fun <T> Sequence<T>.takeWhileInclusive(predicate: (T) -> Boolean) = sequence {
+    with(iterator()) {
+        while (hasNext()) {
+            val next = next()
+            yield(next)
+            if (!predicate(next)) break
+        }
+    }
+}
